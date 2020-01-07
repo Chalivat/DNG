53a44fba5b3d1a14b72f8eb53dab8d61b2af70dc
Chalivat <Chalivat.quentin@gmail.com>
2020-01-07 18:42:49 +0100

    Card scriptable and GAMEoBJECT

---
+++ ProtoGrent/Assets/Scripts/Card_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Card_Script : MonoBehaviour
+{
+    public Card card;
+
+    private new string name;
+    private string description;
+    private int damage;
+
+    private int nombrePioche;
+    private int nombreDefausse;
+
+    public Text cardName;
+    public Text cardDamage;
+    public Text cardDescription;
+
+    void Start()
+    {
+        name = card.name;
+        description = card.name;
+        damage = card.damage;
+
+        nombrePioche = card.nombrePioche;
+        nombreDefausse = card.nombreDefausse;
+
+        cardName.text = name;
+        cardDamage.text = damage.ToString();
+        cardDescription.text = description;
+    }
+    
+    void Update()
+    {
+        
+    }
+}
