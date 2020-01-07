eb26b2a2dd282a115a57fd1aa89c3cdaa29b88d2
elianBroux74 <elianbroux@gmail.com>
2020-01-07 16:05:31 +0100

    Add unity projet

---
+++ ProtoGrent/Assets/Scripts/Board_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Board_Script : MonoBehaviour
+{
+    public Case_Script[,] allCase;
+
+    private void OnEnable()
+    {
+
+    }
+
+
+}
---
+++ ProtoGrent/Assets/Scripts/Case_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Case_Script : MonoBehaviour
+{
+    public Vector2 pos;
+
+    public bool Check(int type)
+    {
+        if(pos.y == type)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void UpdateBoard(Vector2 pos)
+    {
+        
+    }
+}
