75d90d788da506e534a8431bf10dc81708852bc2
elianBroux74 <elianbroux@gmail.com>
2020-01-07 22:36:06 +0100

    highlight lignes

--- ProtoGrent/Assets/Scripts/Case_Script.cs
+++ ProtoGrent/Assets/Scripts/Case_Script.cs
@@ -6,6 +6,8 @@ public class Case_Script : MonoBehaviour
 {
     public Vector2 pos;
 
+    public bool isEmpty = true;
+
     public bool Check(int type)
     {
         if(pos.y == type)
@@ -15,7 +17,16 @@ public class Case_Script : MonoBehaviour
         return false;
     }
 
-    public void UpdateBoard(Vector2 pos)
+    public void PlacerCarte()
+    {
+
+        isEmpty = true;
+        //set scriptable object value
+
+        UpdateBoard();
+    }
+
+    void UpdateBoard()
     {
         GetComponentInParent<Board_Script>().allCase[(int)pos.x,(int)pos.y] = this;
     }
