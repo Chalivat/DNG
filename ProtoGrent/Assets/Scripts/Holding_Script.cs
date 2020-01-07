75e8dc84319356250c40530b03bc9394ee20d3cb
Chalivat <Chalivat.quentin@gmail.com>
2020-01-07 17:02:53 +0100

    Holding Script

---
+++ ProtoGrent/Assets/Scripts/Holding_Script.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holding_Script : MonoBehaviour
+{
+    private GameObject Card, Case;
+    private bool canPlayCard;
+    public Camera cam;
+
+    void Start()
+    {
+        
+    }
+   
+    void Update()
+    {
+        CheckForCase();
+    }
+
+    void CheckForCase()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 500f))
+        {
+            if (hit.transform.CompareTag("Case"))
+            {
+                Case = hit.transform.gameObject;
+                //canPlayCard = Case.Check(Card);
+            }
+        }
+
+    }
+
+    void PlayCard()
+    {
+        if (Case)
+        {
+            if (canPlayCard)
+            {
+                //Case.PlayCard
+            }
+        }
+    }
+}
