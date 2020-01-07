4beff3b23aafdf99f11167b37381b66cccf84c6a
Chalivat <Chalivat.quentin@gmail.com>
2020-01-07 17:19:21 +0100

    Card Scriptable object created

---
+++ ProtoGrent/Assets/Cards/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public int damage;
+
+    public int nombrePioche;
+    public int nombreDefausse;
+}
