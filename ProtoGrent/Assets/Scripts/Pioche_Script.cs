09f6b808b87f6b6228ec497670abb240fe1ef03f
LeoDiringer <52820052+LeoDiringer@users.noreply.github.com>
2020-01-08 11:22:14 +0100

    Script Maon et Pioche

---
+++ ProtoGrent/Assets/Scripts/Pioche_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pioche_Script : MonoBehaviour
+{
+    public List<string> carte = new List<string>();
+    public int nombrePioche;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowCarte(nombrePioche);
+        }
+    }
+
+    void ShowCarte(int nombre)
+    {
+        for (int i = 0; i < nombre; i++)
+        {
+            int rnd = Random.Range(0, carte.Count - 1);
+            print(carte[rnd]);
+            carte.RemoveAt(rnd);
+        }
+    }
+}
