f27131767aa2def7734f728b7dbb9c32e8435401
LeoDiringer <52820052+LeoDiringer@users.noreply.github.com>
2020-01-10 15:20:38 +0100

    qd

---
+++ ProtoGrent/Assets/Scripts/ChangeEvent_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeEvent_Script : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void AddHoldingEvent()
+    {
+
+    }
+}
